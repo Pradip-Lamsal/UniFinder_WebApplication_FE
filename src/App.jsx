2aced983cc666f8068526b7dddfaacae2fd76b32
Pradip Lamsal <pradiplamsal80@gmail.com>
2025-02-27 13:35:16 +0545

    assets

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import SmartSearch from "./pages/SmartSearch";
 import TopUniversities from "./pages/TopUniversities";
-
+import UniversityDetails from "./pages/UniversityDetails";
 function App() {
   return (
     <Router>
@@ -19,6 +19,7 @@ function App() {
         <Route path="/comprehensive-insights" element={<ComprehensiveInsights />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/university/:id" element={<UniversityDetails />} />
       </Routes>
       <Footer />
     </Router>
